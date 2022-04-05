a162e10ca3290ae32dfa94f650b2e84f6c4ba45e
marko.stevic <marko.stevic@zeastim.com>
2022-04-05 02:12:03 +0200

    Backend Cleanup

--- backend/app/Jobs/BidJob.php
+++ backend/app/Jobs/BidJob.php
@@ -26,6 +26,7 @@ class BidJob implements ShouldQueue, ShouldBeUnique
     public $userId;
     public $auctionItemId;
     public $requestType;
+    public $message;
     /**
      * Create a new job instance.
      *
@@ -36,6 +37,7 @@ class BidJob implements ShouldQueue, ShouldBeUnique
         $this->userId = $userId;
         $this->auctionItemId = $auctionItemId;
         $this->requestType = $requestType;
+        $this->message = [];
     }
 
     /**
@@ -51,134 +53,40 @@ class BidJob implements ShouldQueue, ShouldBeUnique
         $auctionItem = AuctionItem::findOrFail($this->auctionItemId);
 
         if (now() >= Carbon::parse($auctionItem->auction_start)) {
-            $auctionItem->update(['is_active' => false]);
-            $user = User::findOrFail($this->userId);
-
-            $latestItem = ItemBiddingHistory::where('auction_item_id', $this->auctionItemId)->latest()->first();
-
-            $newAmount = $user->account - $latestItem->bid_amount;
-
-            $user->update(['account' => $newAmount]);
-
-
-            $autoBids = AutoBid::where('auction_item_id', $this->auctionItemId)->get();
-            foreach ($autoBids as $bid) {
-                $bid->update(['is_active' => false]);
-            }
-            $message = [
-                'auction_end' => 'Auction for this item has ended'
-            ];
-
-            $items = ItemBiddingHistory::where('auction_item_id', $this->auctionItemId)->where('user_id', '!=', -1)->get()->pluck('user_id')->unique();
-
-            $messageAuctionOver = "The auction for the item " . $auctionItem->name . "IS OVER";
-
-            foreach ($items as $item) {
-                $notification = UserNotifications::where('user_id', $item)->where('auction_item_id', $this->auctionItemId)->where('type', 2)->get()->first();
-
-                if ($notification == null) {
-                    $notification = UserNotifications::create([
-                        'user_id' => $item,
-                        'auction_item_id' => $this->auctionItemId,
-                        'type' => 2,
-                        'sent_notification' => 1,
-                        'message' => $messageAuctionOver
-                    ]);
-                    NotificationEvent::dispatch($message, $item, $notification);
-                } else {
-                    if ($notification->sent_notification == 0) {
-                        $notification->update([
-                            'user_id' => $item,
-                            'auction_item_id' => $this->auctionItemId,
-                            'type' => 2,
-                            'sent_notification' => 1,
-                            'message' => $messageAuctionOver
-                        ]);
-                        NotificationEvent::dispatch($message, $item, $notification);
-                    }
-                }
-            }
+            // auction is over
+            $this->handleAuctionEnd($auctionItem);
         } else {
             $itemBidHistoryCurrent = ItemBiddingHistory::where('auction_item_id', $this->auctionItemId)->latest()->first();
 
-            $flag = 0;
+            $userCanBid = $this->handleUserCanBid($itemBidHistoryCurrent);
 
-
-            if ($itemBidHistoryCurrent->user_id == $this->userId) {
-                $message = [
-                    'message' => 'You cant bid against yourself!',
-                    'user_id' => $this->userId,
-                ];
-                $flag = 1;
-            }
-
-
-
-            if ($flag == 0) {
+            if ($userCanBid) {
 
                 $newBidAmount = $itemBidHistoryCurrent->bid_amount + 1;
 
                 if ($this->requestType == 1) {
                     $hasFunds = $this->canUserAutoBid();
 
-                    if ($hasFunds) {
-                        $itemBidHistoryUser = ItemBiddingHistory::create(['auction_item_id' => $this->auctionItemId, 'user_id' => $this->userId, 'bid_amount' => $newBidAmount]);
-
-                        $message = [
-                            'item_bidding_history' => $itemBidHistoryUser,
-                            'user' => $itemBidHistoryUser->user,
-                            'request_type' => $this->requestType
-                        ];
-                    } else {
-                        $user = User::find($this->userId);
-                        $message = [
-                            'does_not_have_funds' => 'You are exciding the limit you provided in Settings',
-                            'user' => $user
-                        ];
-                    }
+                    $this->setMessageForAutoBidding($hasFunds, $newBidAmount);
                 } else {
 
                     $user = User::findOrFail($this->userId);
-                    $items = ItemBiddingHistory::where('user_id', $this->userId)->get()->pluck('auction_item_id')->unique();
 
-                    $auctionItems = AuctionItem::where('is_active', 0)->whereIn('id', $items)->get()->pluck('id');
+                    $sum = $this->calculateAllReservedFunds();
 
-                    $sum = 0;
-                    foreach ($auctionItems as $item) {
-                        $sum += ItemBiddingHistory::where('user_id', $this->userId)->where('auction_item_id', $item)->get()->max('bid_amount');
-                    }
-
-                    if ($user->account <= $sum) {
-                        $message = [
-                            'not_enough_funds' => 'You currently do not have enought funds on your account.'
-                        ];
-                    } else {
-                        $itemBidHistoryUser = ItemBiddingHistory::create(['auction_item_id' => $this->auctionItemId, 'user_id' => $this->userId, 'bid_amount' => $newBidAmount]);
-
-                        $message = [
-                            'item_bidding_history' => $itemBidHistoryUser,
-                            'user' => $itemBidHistoryUser->user,
-                            'request_type' => $this->requestType
-                        ];
-                    }
+                    $this->setMessageForBid($user, $sum, $newBidAmount);
                 }
             }
         }
 
-        BidEvent::dispatch($message, $this->auctionItemId);
+        BidEvent::dispatch($this->message, $this->auctionItemId);
     }
 
     private function canUserAutoBid()
     {
         $user = User::findOrFail($this->userId);
-        $items = ItemBiddingHistory::where('user_id', $this->userId)->get()->pluck('auction_item_id')->unique();
-
-        $auctionItems = AuctionItem::where('is_active', true)->whereIn('id', $items)->get()->pluck('id');
-
-        $sum = 0;
-        foreach ($auctionItems as $item) {
-            $sum += ItemBiddingHistory::where('user_id', $this->userId)->where('auction_item_id', $item)->get()->max('bid_amount');
-        }
+        $auctionItem = AuctionItem::findOrFail($this->auctionItemId);
+        $sum = $this->calculateAllReservedFunds();
 
         $avalaibleFunds = $user->settings->maximum_bid_amount;
 
@@ -192,36 +100,134 @@ class BidJob implements ShouldQueue, ShouldBeUnique
         }
 
         $calculatedBid = ceil(($user->settings->bid_alert_notification / 100) * $user->settings->maximum_bid_amount);
+        $messageForUser = "You have crossed the limit of " . $user->settings->bid_alert_notification . " (%) for item " . $auctionItem->name;
 
         if ($sum >= $calculatedBid) {
-            $notification = UserNotifications::where('user_id', $this->userId)->where('auction_item_id', $this->auctionItemId)->where('type', 1)->get()->first();
-
-            $auctionItem = AuctionItem::findOrFail($this->auctionItemId);
-
-            $message = "You have crossed the limit of " . $user->settings->bid_alert_notification . "% for " . $auctionItem->name;
-
-            if ($notification == null) {
-                $notification = UserNotifications::create([
-                    'user_id' => $this->userId,
-                    'auction_item_id' => $this->auctionItemId,
-                    'type' => 1,
-                    'sent_notification' => 1,
-                    'message' => $message
-                ]);
-                NotificationEvent::dispatch($message, $this->userId, $notification);
-            } else {
-                if ($notification->sent_notification == 0) {
-                    $notification->update([
-                        'user_id' => $this->userId,
-                        'auction_item_id' => $this->auctionItemId,
-                        'type' => 1,
-                        'sent_notification' => 1,
-                        'message' => $message
-                    ]);
-                    NotificationEvent::dispatch($message, $this->userId, $notification);
-                }
-            }
+            $this->notifyUser($user->id, $messageForUser, $this->message);
         }
         return true;
+    }
+
+    private function handleAuctionEnd($auctionItem)
+    {
+        $auctionItem->update(['is_active' => false]);
+        $user = User::findOrFail($this->userId);
+
+        $latestItem = ItemBiddingHistory::where('auction_item_id', $this->auctionItemId)->latest()->first();
+
+        $newAmount = $user->account - $latestItem->bid_amount;
+
+        $user->update(['account' => $newAmount]);
+
+
+        $this->disableAutoBids();
+        $this->message = [
+            'auction_end' => 'Auction for this item has ended'
+        ];
+
+        $usersId = ItemBiddingHistory::where('auction_item_id', $this->auctionItemId)->where('user_id', '!=', -1)->get()->pluck('user_id')->unique();
+
+        $messageForUser = "The auction for the item " . $auctionItem->name . "IS OVER";
+
+        foreach ($usersId as $userId) {
+            $this->notifyUser($userId, $messageForUser, $this->message);
+        }
+    }
+
+    private function notifyUser($userId, $messageForUser, $message)
+    {
+        $notification = UserNotifications::where('user_id', $userId)->where('auction_item_id', $this->auctionItemId)->where('type', $this->requestType)->get()->first();
+
+        if ($notification == null) {
+            $notification = UserNotifications::create([
+                'user_id' => $userId,
+                'auction_item_id' => $this->auctionItemId,
+                'type' => $this->requestType,
+                'sent_notification' => 1,
+                'message' => $messageForUser
+            ]);
+            NotificationEvent::dispatch($message, $userId, $notification);
+        } else {
+            if ($notification->sent_notification == 0) {
+                $notification->update([
+                    'user_id' => $userId,
+                    'auction_item_id' => $this->auctionItemId,
+                    'type' => $this->requestType,
+                    'sent_notification' => 1,
+                    'message' => $messageForUser
+                ]);
+                NotificationEvent::dispatch($message, $userId, $notification);
+            }
+        }
+    }
+
+    private function disableAutoBids()
+    {
+        $autoBids = AutoBid::where('auction_item_id', $this->auctionItemId)->get();
+        foreach ($autoBids as $bid) {
+            $bid->update(['is_active' => false]);
+        }
+    }
+
+    private function handleUserCanBid($itemBidHistoryCurrent)
+    {
+        if ($itemBidHistoryCurrent->user_id == $this->userId) {
+            $this->message = [
+                'message' => 'You cant bid against yourself!',
+                'user_id' => $this->userId,
+            ];
+            return false;
+        }
+        return true;
+    }
+
+    private function setMessageForAutoBidding($hasFunds, $newBidAmount)
+    {
+        if ($hasFunds) {
+            $itemBidHistoryUser = ItemBiddingHistory::create(['auction_item_id' => $this->auctionItemId, 'user_id' => $this->userId, 'bid_amount' => $newBidAmount]);
+
+            $this->message = [
+                'item_bidding_history' => $itemBidHistoryUser,
+                'user' => $itemBidHistoryUser->user,
+                'request_type' => $this->requestType
+            ];
+        } else {
+            $user = User::find($this->userId);
+            $this->message = [
+                'does_not_have_funds' => 'You are exciding the limit you provided in Settings',
+                'user' => $user
+            ];
+        }
+    }
+
+    private function calculateAllReservedFunds()
+    {
+        $items = ItemBiddingHistory::where('user_id', $this->userId)->get()->pluck('auction_item_id')->unique();
+
+        $auctionItems = AuctionItem::where('is_active', true)->whereIn('id', $items)->get()->pluck('id');
+
+        $sum = 0;
+        foreach ($auctionItems as $item) {
+            $sum += ItemBiddingHistory::where('user_id', $this->userId)->where('auction_item_id', $item)->get()->max('bid_amount');
+        }
+
+        return $sum;
+    }
+
+    public function setMessageForBid($user, $sum, $newBidAmount)
+    {
+        if ($user->account <= $sum) {
+            $this->message = [
+                'not_enough_funds' => 'You currently do not have enought funds on your account.'
+            ];
+        } else {
+            $itemBidHistoryUser = ItemBiddingHistory::create(['auction_item_id' => $this->auctionItemId, 'user_id' => $this->userId, 'bid_amount' => $newBidAmount]);
+
+            $this->message = [
+                'item_bidding_history' => $itemBidHistoryUser,
+                'user' => $itemBidHistoryUser->user,
+                'request_type' => $this->requestType
+            ];
+        }
     }
 }
